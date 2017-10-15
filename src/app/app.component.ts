923d5cbad05ac0b082fcae92d65872f0f6b6b86a
Leonardo P Ferreira <lpfw82@gmail.com>
2017-10-15 18:15:34 -0200

    Push notification adicionado. Entretanto ainda todos os dispositivos recebem a notificacao.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { LoginPage } from './../pages/login/login';
+import { FCM } from '@ionic-native/fcm';
 
 @Component({
   templateUrl: 'app.html'
@@ -12,12 +13,27 @@ import { LoginPage } from './../pages/login/login';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private fcm: FCM) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      
+      if (platform.is('cordova')) {
+        fcm.onNotification().subscribe(data=>{
+          if (data.wasTapped) {
+
+          } else {
+
+          };
+        })
+
+        //Note that this callback will be fired everytime a new token is generated, including the first time.
+        fcm.onTokenRefresh().subscribe(token=>{
+
+        });
+      }
     });
   }
 }
